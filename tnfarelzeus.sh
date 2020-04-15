50eee5fd8025615a83d9c9c3c7bc3396e56e60ad
FarelZeus <63641611+FarelZeus@users.noreply.github.com>
2020-04-15 10:45:16 +0700

    Add files via upload

---
+++ tnfarelzeus.sh
@@ -0,0 +1,55 @@
+
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+echo
+sleep 3
+toilet -f big -F gay FARELZEUS
+echo
+sleep 2
+toilet -f big -F gay TOOLS
+echo $green"Versi 1.1"
+echo
+echo $blue"Whatsapp : 081377804838"
+echo $green"Yutube :ZeusGTPS"
+echo $red"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo
+sleep 3
+echo $yellow"Silahkan Dipilih Cok"
+echo $red"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo
+echo $purple"1.Ngestabilkan Sinyal Loe"
+echo $red"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo $cyan"2.Sadap WhatsApp (soon)"
+echo $red"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo $blue"3.Cek IP Korban Via Notelp (soon)"
+echo $red"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo $blue"00.Keluar Cok"
+echo $red"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo
+echo $red
+read -p " Pilih Mana Loe : " guys
+if [ $guys = 1 ] || [ $guys = 1 ]
+then
+clear
+toilet -f big -F gay FARELZEUS
+echo $yellow"Silahkan Pilih Cok"
+sleep 3
+ping -s3000 192.168.100.32
+ping -s3000 192.168.100.32
+ping -s3000 192.168.100.32
+fi
+
+if [ $guys = 00 ] || [ $guys = 00 ]
+then
+figlet "BYE"
+echo $cyan"makasih udah pake ini tools"
+sleep 3
+exit
+fi
+clear
